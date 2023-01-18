d73d5eee2b52b4af8092da258a03a8c3eeb9f67a
Matthew <34130768+SpaceGarten@users.noreply.github.com>
2023-01-18 15:27:27 -0800

    Add files via upload

---
+++ Assignment/assignment1.js
@@ -0,0 +1,51 @@
+// Question 1  
+
+function studentWithHighestMarks(array) {
+    let highestMarks = 0;
+    let highestStudent = "";
+
+    array.forEach((student) => {
+        let totalMarks = student.marks.reduce((acc, mark) => acc + mark, 0);
+        if (totalMarks > highestMarks) {
+            highestMarks = totalMarks;
+            highestStudent = student.name;
+        }
+    });
+
+    return highestStudent;
+}
+
+
+let student = [{ name: "Daniel", email: "daniel@gmail.com", marks: [80, 60, 50, 70, 95] },
+{ name: "Mark", email: "mark@gmail.com", marks: [99, 40, 84, 72, 60] },
+{ name: "Stacy", email: "stacy@gmail.com", marks: [8, 30, 11, 0, 20] },
+{ name: "Geri", email: "geri@gmail.com", marks: [100, 99, 95, 85, 99] }
+];
+
+
+console.log(studentWithHighestMarks(student));
+
+
+
+// Question 2
+let array = [20, -2, 4, -11, 0, 25]
+
+
+let minimumValue = array.reduce(function (acc, cV) {
+    return acc < cV ? acc : cV;
+}, Infinity);
+
+
+console.log(minimumValue);
+
+
+
+// Question 3
+function lengthOfString(string) {
+    let arr = string.split("");
+    return arr.length;
+
+}
+
+let strName = "helloworld";
+console.log(lengthOfString(strName));
