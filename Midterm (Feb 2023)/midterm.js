a9f2ad6803b895428aba3a59e9ea71f752233ac4
Muochu Hu <34130768+SpaceGarten@users.noreply.github.com>
2023-02-21 10:26:15 -0800

    Update midterm.js

--- Midterm (Feb 2023)/midterm.js
+++ Midterm (Feb 2023)/midterm.js
@@ -6,15 +6,28 @@ const searchBar = document.getElementById('search-bar');
 const movieContainer = document.getElementById('movie-container');
 
 // create a template for the movie tile
-const movieTileTemplate = (title, releaseDate, description, genre, imageUrl) => `
+const movieTileTemplate = (title, releaseDate, description, genres, imageUrl) => `
   <div class="movie-tile">
     <img src="${imageUrl}" alt="${title}">
     <h3>${title}</h3>
     <p>Released: ${releaseDate}</p>
-    <p>Genre: ${genre}</p>
+    <p>Genres: ${genres.join(', ')}</p>
     <p>${description}</p>
   </div>
 `;
+
+// fetch the list of genres
+const fetchGenres = () => {
+  const url = `https://api.themoviedb.org/3/genre/movie/list?api_key=${apiKey}`;
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => data.genres)
+    .catch(error => {
+      console.error('Error fetching genres:', error);
+      return [];
+    });
+};
 
 // fetch data from the API and update the HTML with the movie data
 const fetchMovies = (searchQuery = '') => {
@@ -30,21 +43,30 @@ const fetchMovies = (searchQuery = '') => {
       // clear the previous movie tiles
       movieContainer.innerHTML = '';
 
-      movies.forEach(movie => {
-        // extract the relevant movie data
-        const title = movie.title;
-        const releaseDate = movie.release_date;
-        const description = movie.overview;
-        const genre = movie.genre_ids;
-        const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+      // fetch the list of genres and map the genre ids to their names
+      fetchGenres().then(genres => {
+        const genreMap = {};
+        genres.forEach(genre => {
+          genreMap[genre.id] = genre.name;
+        });
 
-        // create a new movie tile and fill in the data
-        const movieTile = document.createElement('div');
-        movieTile.classList.add('movie-tile');
-        movieTile.innerHTML = movieTileTemplate(title, releaseDate, description, genre, imageUrl);
+        movies.forEach(movie => {
+          // extract the relevant movie data
+          const title = movie.title;
+          const releaseDate = movie.release_date;
+          const description = movie.overview;
+          const genreIds = movie.genre_ids;
+          const genres = genreIds.map(id => genreMap[id] || 'Unknown');
+          const imageUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
 
-        // add the movie tile to the container
-        movieContainer.appendChild(movieTile);
+          // create a new movie tile and fill in the data
+          const movieTile = document.createElement('div');
+          movieTile.classList.add('movie-tile');
+          movieTile.innerHTML = movieTileTemplate(title, releaseDate, description, genres, imageUrl);
+
+          // add the movie tile to the container
+          movieContainer.appendChild(movieTile);
+        });
       });
     })
     .catch(error => {
